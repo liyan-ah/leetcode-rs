d62a0cba243f91963f4882480641d6faa14c76b2
liyan-ah <liyan-ah@foxmail.com>
2024-07-26 21:59:15 +0800

    2024/07/26

--- le-co/src/mt.rs
+++ le-co/src/mt.rs
@@ -791,3 +791,34 @@ impl Solution {
         vec![answer_0, answer_1]
     }
 }
+
+impl Solution {
+    pub fn is_even(num: i32) -> bool {
+        if num < 10 {
+            return false;
+        }
+        if num < 100 {
+            return true;
+        }
+        if num < 1000 {
+            return false;
+        }
+        if num < 10000 {
+            return true;
+        }
+        if num < 100000 {
+            return false;
+        }
+        true
+    }
+
+    pub fn find_numbers(nums: Vec<i32>) -> i32 {
+        let mut count = 0;
+        for num in nums.iter() {
+            if Self::is_even(*num) {
+                count += 1;
+            }
+        }
+        count
+    }
+}
