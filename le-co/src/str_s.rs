5121662f2fa5818ed30e28a910069c212cb8f929
liyan-ah <liyan-ah@foxmail.com>
2024-08-13 22:36:19 +0800

    2024/08/13

--- le-co/src/str_s.rs
+++ le-co/src/str_s.rs
@@ -592,3 +592,27 @@ impl Solution {
         re_order.join(" ")
     }
 }
+
+impl Solution {
+    pub fn are_occurrences_equal(s: String) -> bool {
+        let mut freq = std::collections::HashMap::new();
+        for c in s.chars().into_iter() {
+            if let Some(v) = freq.get_mut(&c) {
+                *v += 1;
+            } else {
+                freq.insert(c, 1);
+            }
+        }
+        let mut cons = 0;
+        for (_, v) in freq.iter() {
+            if cons != 0 && cons != *v {
+                return false;
+            }
+            if cons == 0 {
+                cons = *v;
+            }
+        }
+
+        true
+    }
+}
