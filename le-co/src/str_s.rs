72d484c29d3e7c2667a08f366f9f2d162895fc06
liyan-ah <liyan-ah@foxmail.com>
2024-08-14 23:02:04 +0800

    2024/08/14

--- le-co/src/str_s.rs
+++ le-co/src/str_s.rs
@@ -616,3 +616,15 @@ impl Solution {
         true
     }
 }
+
+impl Solution {
+    pub fn generate_the_string(n: i32) -> String {
+        if n % 2 == 1 {
+            return vec!['a'; n as usize].iter().collect();
+        }
+        let mut res = vec!['a'; (n - 1) as usize];
+        res.push('b');
+
+        res.iter().collect()
+    }
+}
